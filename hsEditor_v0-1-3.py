1cd46990b8514d62da0d69e361ca27434d367022
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-11-02 11:28:54 +0800

    Update hsEditor_v0-1-3.py

--- hsEditor_v0-1-3.py
+++ hsEditor_v0-1-3.py
@@ -1,7 +1,54 @@
-value = input('highscore value to be edited to: ')
+import os, time
 
-f = open('.highscore.txt', 'a+')
-f.write(value)
-f.close()
+def clear(): os.system('cls') if os.name == 'nt' else os.system('clear')
 
-print('Your highscore has been edited successfully.')
+def qwerty():
+    global value
+    value = input('Select actions:\n1. Clear all scores\n2. Edit highscore value\n3. clean up scores\n4. Exit script\n\n>> ')
+    return value
+
+while True:
+    time.sleep(1)
+    qwerty()
+    if value == '1':
+        clear()
+        time.sleep(1)
+        os.remove('.highscore.txt')
+        print('Removing local scores...')
+        time.sleep(3)
+        print('Your local scores have been cleared successfully.\n--End of session, please wait--')
+        time.sleep(5)
+        clear()
+    elif value == '2':
+        clear()
+        time.sleep(1)
+        ed = input('Enter highscore to be appended.\n\n>> ')
+        clear()
+        print('Loading file...')
+        f = open('.highscore.txt', 'a+')
+        f.write(f'{ed}\n')
+        f.close()
+        time.sleep(3)
+        print('Your local highscore has been edited successfully.\n--End of session, please wait--')
+        time.sleep(5)
+        clear()
+    elif value == '3':
+        clear()
+        time.sleep(1)
+        print('Cleaning up zeros...')
+        with open('.highscore.txt', 'r+') as fb:
+            lines = fb.readlines()
+            fb.seek(0)
+            fb.truncate()
+            for i in lines:
+                if i.strip('\n') != "0": fb.write(i)
+        time.sleep(3)
+        print('Your scores have been cleaned up successfully.\n--End of session, please wait--')
+        time.sleep(5)
+        clear()
+    elif value == '4':
+        clear()
+        print('Script will exit soon...')
+        time.sleep(3)
+        exit()
+    else: print('Invalid action. Please try again and enter a correct action. (1/2/3)')
